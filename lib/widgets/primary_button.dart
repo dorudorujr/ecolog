06c24d4033771a3fa445a2fa84e7bc85f5038e8c
杉岡成哉 <a1209s518n928@gmail.com>
2021-05-29 01:02:32 +0900

    add PrimaryButton&SecondaryButton

---
+++ lib/widgets/primary_button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class PrimaryButton extends StatelessWidget {
+  const PrimaryButton({
+    Key? key,
+    required this.width,
+    required this.height,
+    required this.onPressed,
+    required this.text,
+  }) : super(key: key);
+
+  final double width;
+  final double height;
+  final VoidCallback onPressed;
+  final String text;
+  
+  final primaryColor = const Color.fromRGBO(77, 181, 171, 1.0);
+  final onPrimaryColor = Colors.white;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      height: height,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.normal
+          ),
+        ),
+        style: ElevatedButton.styleFrom(
+          primary: primaryColor,
+          onPrimary: onPrimaryColor,
+        ),
+      ),
+    );
+  }
+}
