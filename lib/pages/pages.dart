36d88e4f5e4ae54911496738efd40ccd4e3557a6
杉岡成哉 <a1209s518n928@gmail.com>
2021-05-28 13:04:08 +0900

    add splash_page.dart

---
+++ lib/pages/pages.dart
@@ -0,0 +1,1 @@
+export 'package:ecolog/pages/splash_page.dart';
---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+import 'package:ecolog/application_model/firebases/firestore/forced_update/forced_update.dart';
+import 'package:ecolog/application_model/models/models.dart';
+import 'package:ecolog/widgets/widgets.dart';
+import 'package:ecolog/controllers/splash/splash.dart';
+
+class SplashPage extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          /// 強制アップデート有無
+          child: splashCoordinator(useProvider(splashControllerProvider))
+        ),
+      ),
+    );
+  }
+
+  /// splash画面の遷移処理
+  Widget splashCoordinator(SplashStatusType type) {
+    switch(type) {
+      case SplashStatusType.forcibly:
+        return VersionCheckDialog();
+      default:
+        return Center();  
+    }
+  }
+}
