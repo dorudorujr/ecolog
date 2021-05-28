d0f8dd44a4a87ff73f43704b847bae1d0444006b
杉岡成哉 <a1209s518n928@gmail.com>
2021-05-28 14:06:57 +0900

    不要なファイル削除

--- lib/application_model/firebases/remote_config/remote_config.dart
+++
@@ -1,2 +0,0 @@
-export 'package:ecolog/application_model/firebases/remote_config/update_requester_provider.dart';
-export 'package:ecolog/application_model/firebases/remote_config/remote_config_provider.dart';
--- lib/application_model/models/models.dart
+++ lib/application_model/models/models.dart
@@ -1,2 +1,1 @@
-export 'package:ecolog/application_model/models/update_request_type.dart';
 export 'package:ecolog/application_model/models/splash_status_type.dart';
