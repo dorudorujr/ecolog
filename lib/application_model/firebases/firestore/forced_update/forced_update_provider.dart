d79614278cf96dd17985c6fe4b7fcf3e2547d0d9
杉岡成哉 <a1209s518n928@gmail.com>
2021-05-28 14:05:32 +0900

    providerからはアップデート情報を取得するのみに修正

--- lib/application_model/firebases/firestore/forced_update/forced_update_provider.dart
+++ lib/application_model/firebases/firestore/forced_update/forced_update_provider.dart
@@ -1,34 +1,13 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:package_info/package_info.dart';
-import 'package:version/version.dart';
 
 import 'package:ecolog/application_model/entities/update_info_entity/update_info_entity.dart';
-import 'package:ecolog/application_model/models/models.dart';
 
-/// 強制アップデートProvider
-/// TODO: error時の処理対応
-final forcedUpdateProvider = FutureProvider<UpdateRequestType>((ref) async {
-  // 現在のアプリバージョンを取得するためにPackageInfoを利用
-  final appPackageInfo = await PackageInfo.fromPlatform();
-
+/// 強制アップデート情報取得Provider
+final forcedUpdateProvider = FutureProvider<UpdateInfoEntity>((ref) async {
   final collection = await FirebaseFirestore.instance.collection('version').get();
   final data = collection.docs.first.data();
-  final updateInfo = UpdateInfoEntity.fromJson(data);
 
-  final enabledAt = updateInfo.enabledAt;
-  final requiredVersion = Version.parse(updateInfo.requiredVersion);
-  final currentVersion = Version.parse(appPackageInfo.version);
-  // 現在のバージョンより新しいバージョンが指定されているか
-  final hasNewVersion = requiredVersion > currentVersion;
-  // 強制アップデート有効期間内かどうか
-  final isEnabled = enabledAt.compareTo(DateTime.now()) < 0;
-
-  if (!isEnabled && !hasNewVersion) {
-    // 有効期間外、もしくは新しいバージョンは無い
-    return UpdateRequestType.not;
-  }
-  return updateInfo.canCancel
-      ? UpdateRequestType.cancelable
-      : UpdateRequestType.forcibly;
+  /// TODO: error時の処理対応
+  return UpdateInfoEntity.fromJson(data);
 });
--- lib/application_model/models/models.dart
+++ lib/application_model/models/models.dart
@@ -1,1 +1,2 @@
 export 'package:ecolog/application_model/models/update_request_type.dart';
+export 'package:ecolog/application_model/models/splash_status_type.dart';
