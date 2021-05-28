b38b7a1d182e9a3899c2fd19cd91af0c9216b603
杉岡成哉 <a1209s518n928@gmail.com>
2021-05-28 15:02:40 +0900

    不要なコメント削除

--- lib/controllers/splash/splash_controller.dart
+++ lib/controllers/splash/splash_controller.dart
@@ -41,8 +41,7 @@ class SplashController extends StateNotifier<SplashStatusType> {
     final hasNewVersion = requiredVersion > currentVersion;
     // 強制アップデート有効期間内かどうか
     final isEnabled = enabledAt.compareTo(DateTime.now()) > 0;
-
-    print('hasNewVersion:${DateTime.now()},${enabledAt}');
+    
     return hasNewVersion && isEnabled;
   }
 }
