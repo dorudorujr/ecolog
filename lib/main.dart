02c9c8270709e875251d31f02ad533df98a5c689
杉岡成哉 <a1209s518n928@gmail.com>
2021-05-28 13:04:28 +0900

    splash画面実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 
-import 'package:ecolog/application_model/firebases/remote_config/remote_config.dart';
-import 'package:ecolog/application_model/firebases/firestore/forced_update/forced_update.dart';
-import 'package:ecolog/widgets/widgets.dart';
+import 'package:ecolog/pages/pages.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,26 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends HookWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('バージョンアップダイアログ')),
-      body: SafeArea(
-        child: Center(
-          // FutureProviderなので、 `when` で loading, error, data のハンドリングができる
-          child: useProvider(forcedUpdateProvider).when(
-            loading: () => const CircularProgressIndicator(),
-            error: (error, stack) => ErrorWidget(error),
-            data: (requestType) => VersionCheckDialog(requestType: requestType),
-          ),
-        ),
-      ),
+      home: SplashPage(),
     );
   }
 }
