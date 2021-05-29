5f310b7fec60186bd3437384ea43fa9ea28527a3
杉岡成哉 <a1209s518n928@gmail.com>
2021-05-29 22:07:34 +0900

    constructorでの処理をawaitさせるよう修正

--- lib/controllers/splash/splash_controller.dart
+++ lib/controllers/splash/splash_controller.dart
@@ -1,11 +1,11 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:state_notifier/state_notifier.dart';
-import 'package:package_info/package_info.dart';
 import 'package:version/version.dart';
 
 import 'package:ecolog/application_model/models/models.dart';
 import 'package:ecolog/application_model/firebases/firestore/forced_update/forced_update.dart';
 import 'package:ecolog/application_model/entities/update_info_entity/update_info_entity.dart';
+import 'package:ecolog/util/provider/package_info/package_info.dart';
 
 final splashControllerProvider = StateNotifierProvider<SplashController, SplashStatusType>(
     (ref) => SplashController(ref.read),
@@ -13,11 +13,15 @@ final splashControllerProvider = StateNotifierProvider<SplashController, SplashS
 
 class SplashController extends StateNotifier<SplashStatusType> {
   SplashController(this._read) : super(SplashStatusType.load) {
-    _check();
+    () async {
+      await _check();
+    }();
+    //check();
   }
 
   final Reader _read;
 
+  /// testで使用するためにnot private
   Future<void> _check() async {
     final isForcedUpdate = await _isForcedUpdate(await _read(forcedUpdateProvider.future));
 
@@ -30,12 +34,11 @@ class SplashController extends StateNotifier<SplashStatusType> {
 
   /// 強制アップデート有無
   Future<bool> _isForcedUpdate(UpdateInfoEntity updateInfo) async {
-    // 現在のアプリバージョンを取得するためにPackageInfoを利用
-    final appPackageInfo = await PackageInfo.fromPlatform();
+    final version = await _read(packageInfoProvider.future);
 
     final enabledAt = updateInfo.enabledAt;
     final requiredVersion = Version.parse(updateInfo.requiredVersion);
-    final currentVersion = Version.parse(appPackageInfo.version);
+    final currentVersion = Version.parse(version);
 
     // 現在のバージョンより新しいバージョンが指定されているか
     final hasNewVersion = requiredVersion > currentVersion;
