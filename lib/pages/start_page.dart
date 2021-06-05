755f7890d85ae907d9a655f1394455e28def5112
杉岡成哉 <a1209s518n928@gmail.com>
2021-06-05 16:33:15 +0900

    匿名signin実装

--- lib/pages/start_page.dart
+++ lib/pages/start_page.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'package:ecolog/util/const/string/const_string.dart';
 import 'package:ecolog/widgets/widgets.dart';
 import 'package:ecolog/generated/assets.gen.dart';
+import 'package:ecolog/application_model/firebases/auth/anonymous/anonymous.dart';
+import 'package:ecolog/pages/pages.dart';
 
-class StartPage extends StatelessWidget {
+
+class StartPage extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final deviceHeight = MediaQuery.of(context).size.height;
     final deviceCenterHeight = deviceHeight / 2;
+    final anonymousAuthentication = useProvider(anonymousAuthenticationProvider);
 
     return SafeArea(
         child: Column(
@@ -24,7 +29,7 @@ class StartPage extends StatelessWidget {
               width: 275,
               height: 47,
               text: ConstString.startPageStartButton,
-              onPressed: () {},
+              onPressed: () async { await didStartPageSignInButtonPush(context,anonymousAuthentication);},
             ),
             SizedBox(height: 16,),
             SecondaryButton(
@@ -48,5 +53,18 @@ class StartPage extends StatelessWidget {
           ],
         ),
     );
+  }
+
+  //TODO: この辺の遷移周りの関数をどうにかしたい
+  Future<void> didStartPageSignInButtonPush(BuildContext context,AnonymousAuthentication anonymousAuthentication) async {
+    /// TODO: signout機能を実装したら削除する
+    await anonymousAuthentication.signout();
+
+    anonymousAuthentication.authentication().then((user) {
+      Navigator.push(
+        context,
+          MaterialPageRoute(builder: (context) => HomePage(),)
+      );
+    });
   }
 }
