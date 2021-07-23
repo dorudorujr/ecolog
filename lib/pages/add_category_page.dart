417489f2e9250e3a5099425832630d73aba766c8
杉岡成哉 <a1209s518n928@gmail.com>
2021-07-23 18:39:18 +0900

    文言追加

---
+++ lib/pages/add_category_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import 'package:ecolog/widgets/widgets.dart';
+import 'package:ecolog/application_model/models/models.dart';
+import 'package:ecolog/util/const/string/const_string.dart';
+
+class AddCategoryPage extends StatelessWidget {
+  static const routeName = '/add_category_page';
+
+  const AddCategoryPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text(ConstString.addCategoryTitle),
+      ),
+      body: ListView.builder(
+          itemCount: CategoryType.values.length,
+          itemBuilder: (BuildContext context, int index) {
+            return IconCell(
+                title: CategoryType.values[index].categoryName,
+                categoryType: CategoryType.values[index],
+                onTap: (){});
+          }),
+    );
+  }
+}
