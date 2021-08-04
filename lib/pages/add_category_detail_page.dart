8a80defa203aa771533e4cac7bdaa2a4a53be72b
杉岡成哉 <a1209s518n928@gmail.com>
2021-08-04 22:54:37 +0900

    カテゴリー追加画面作成

--- lib/pages/add_category_detail_page.dart
+++ lib/pages/add_category_detail_page.dart
@@ -3,6 +3,12 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'package:ecolog/application_model/models/models.dart';
+import 'package:ecolog/widgets/widgets.dart';
+import 'package:ecolog/util/const/string/const_string.dart';
+import 'package:ecolog/controllers/add_category_detail/add_category_detail.dart';
+import 'package:ecolog/util/extension/extensions.dart';
+import 'package:ecolog/application_model/entities/category/category_entity.dart';
+import 'package:ecolog/pages/pages.dart';
 
 class AddCategoryDetailPage extends HookWidget {
   static const routeName = '/add_category_detail';
@@ -16,8 +22,65 @@ class AddCategoryDetailPage extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    print('testttt:$categoryType');
-    return const Center();
+    final controller = useProvider(addCategoryDetailProvider.notifier);
+    final state = useProvider(addCategoryDetailProvider);
+    final bottomSpace = MediaQuery.of(context).viewInsets.bottom;
+
+    final _categoryNameInputView = InputView(title: ConstString.addCategoryDetailName, decoration: ConstString.addCategoryDetailNameHint);
+    final _coefficientInputView = InputView(title: ConstString.addCategoryDetailCoefficient, defaultValue: categoryType.coefficient.toString(),);
+    final _unitInputView = InputView(title: categoryType.displayUnitText, decoration: '0',);
+
+    return Stack(
+      fit: StackFit.expand,
+      clipBehavior: Clip.hardEdge,
+      children: [
+        Scaffold(
+          resizeToAvoidBottomInset: false,
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            title: const Text(ConstString.categorysTitle),
+          ),
+          body: SingleChildScrollView(
+            reverse: true,
+            child: Padding(
+              padding: EdgeInsets.only(bottom: bottomSpace),
+              child: Column(
+                children: [
+                  const SizedBox(height: 50,),
+                  _categoryNameInputView,
+                  _coefficientInputView,
+                  _unitInputView,
+                  const SizedBox(height: 100,),
+                  PrimaryButton(
+                    width: 275,
+                    height: 47,
+                    text: ConstString.addCategoryDetailAddButton,
+                    onPressed: () async {
+                      final category = CategoryEntity(
+                          categoryName: _categoryNameInputView.getText,
+                          categoryType: categoryType,
+                          defaultValue: int.parse(_unitInputView.getText)
+                      );
+                      await controller.addCategory(category);
+                      //TODO: 正常の遷移に変わったら修正する
+                      //Navigator.popUntil(context, ModalRoute.withName(CategorysPage.routeName));
+                      Navigator.popUntil(context, ModalRoute.withName('/'));
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        FullScreenLoading(isHidden: !state.isLoading,),
+        _showErrorDialogHandler(state.exception),
+      ],
+    );
+  }
+
+  /// ErrorDialog表示判定
+  Widget _showErrorDialogHandler(Exception? exception) {
+    return ErrorDialog(isShow: exception != null,);
   }
 }
 
