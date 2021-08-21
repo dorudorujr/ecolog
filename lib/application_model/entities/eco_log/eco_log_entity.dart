902f9b06b601507ab526d43f6eddecba3ccac82e
杉岡成哉 <a1209s518n928@gmail.com>
2021-08-21 13:55:35 +0900

    CO2排出の記録用エンティティ作成

---
+++ lib/application_model/entities/eco_log/eco_log_entity.dart
@@ -0,0 +1,93 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:uuid/uuid.dart';
+
+import 'package:ecolog/application_model/models/models.dart';
+
+part 'eco_log_entity.freezed.dart';
+part 'eco_log_entity.g.dart';
+
+@freezed
+class EcoLogEntity with _$EcoLogEntity {
+  const factory EcoLogEntity({
+    required String id,
+    required double value,
+    required CategoryType categoryType,
+    required DateTime date,
+  }) = _EcoLogEntity;
+
+  factory EcoLogEntity.fromJson(Map<String, dynamic> json) => _$EcoLogEntityFromJson(json);
+
+  factory EcoLogEntity.toEcoLogEntity({
+    required value,
+    required CategoryType categoryType,
+    int? time,
+    DateTime? date,
+    String? id,
+  })
+  {
+    DateTime propertyDate = DateTime.now();
+    if (date != null) {
+      propertyDate = date;
+    }
+
+    String propertyId = const Uuid().v4();
+    if (id != null) {
+      propertyId = id;
+    }
+
+    if (categoryType == CategoryType.electricity) {
+      assert(time != null);
+      return EcoLogEntity._fromElectricity(
+          unitValue: value,
+          time: time ?? 0,
+          date: propertyDate,
+          id: propertyId);
+    } else {
+      return EcoLogEntity._toEcoLogEntity(
+          unitValue: value,
+          categoryType: categoryType,
+          date: propertyDate,
+          id: propertyId);
+    }
+  }
+
+  //// 電気系のEcoLogEntityを返す
+  factory EcoLogEntity._fromElectricity({
+    required int unitValue,
+    required int time,
+    required DateTime date,
+    required String id,
+  })
+  {
+    //// CO2排出量計算式
+    double calculateElectricityValue({required int unitValue, required int time, required double coefficient}) {
+      return unitValue * time * coefficient;
+    }
+
+    return EcoLogEntity(
+        id: id,
+        value: calculateElectricityValue(unitValue: unitValue, time: time, coefficient: CategoryType.electricity.coefficient),
+        categoryType: CategoryType.electricity,
+        date: date);
+  }
+
+  //// 電気系以外のEcoLogEntityを返す
+  factory EcoLogEntity._toEcoLogEntity({
+    required int unitValue,
+    required CategoryType categoryType,
+    required DateTime date,
+    required String id,
+  })
+  {
+    //// CO2排出量計算式
+    double calculateValue({required int value, required CategoryType categoryType}) {
+      return value * categoryType.coefficient;
+    }
+
+    return EcoLogEntity(
+        id: id,
+        value: calculateValue(value: unitValue, categoryType: categoryType),
+        categoryType: categoryType,
+        date: date);
+  }
+}
---
+++ lib/application_model/entities/eco_log/eco_log_entity.g.dart
@@ -0,0 +1,57 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'eco_log_entity.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_EcoLogEntity _$_$_EcoLogEntityFromJson(Map<String, dynamic> json) {
+  return _$_EcoLogEntity(
+    id: json['id'] as String,
+    value: (json['value'] as num).toDouble(),
+    categoryType: _$enumDecode(_$CategoryTypeEnumMap, json['categoryType']),
+    date: DateTime.parse(json['date'] as String),
+  );
+}
+
+Map<String, dynamic> _$_$_EcoLogEntityToJson(_$_EcoLogEntity instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'value': instance.value,
+      'categoryType': _$CategoryTypeEnumMap[instance.categoryType],
+      'date': instance.date.toIso8601String(),
+    };
+
+K _$enumDecode<K, V>(
+  Map<K, V> enumValues,
+  Object? source, {
+  K? unknownValue,
+}) {
+  if (source == null) {
+    throw ArgumentError(
+      'A value must be provided. Supported values: '
+      '${enumValues.values.join(', ')}',
+    );
+  }
+
+  return enumValues.entries.singleWhere(
+    (e) => e.value == source,
+    orElse: () {
+      if (unknownValue == null) {
+        throw ArgumentError(
+          '`$source` is not one of the supported values: '
+          '${enumValues.values.join(', ')}',
+        );
+      }
+      return MapEntry(unknownValue, enumValues.values.first);
+    },
+  ).key;
+}
+
+const _$CategoryTypeEnumMap = {
+  CategoryType.electricity: 'electricity',
+  CategoryType.water: 'water',
+  CategoryType.gasoline: 'gasoline',
+  CategoryType.cityGas: 'cityGas',
+};
