8d9e8b07f7d5e0077543cd0910b2073071c5a53f
杉岡成哉 <a1209s518n928@gmail.com>
2021-08-25 21:04:51 +0900

    entityを生成する処理を追加

--- lib/controllers/electricity_input/electricity_input_controller.dart
+++ lib/controllers/electricity_input/electricity_input_controller.dart
@@ -3,6 +3,8 @@ import 'package:state_notifier/state_notifier.dart';
 
 import 'package:ecolog/controllers/electricity_input/electricity_input.dart';
 import 'package:ecolog/application_model/entities/eco_log/eco_log_entity.dart';
+import 'package:ecolog/application_model/models/models.dart';
+import 'package:ecolog/util/extension/extensions.dart';
 
 final electricityInputProvider = StateNotifierProvider<ElectricityInputController, ElectricityInputState>(
       (ref) => ElectricityInputController(ref.read),
@@ -13,9 +15,21 @@ class ElectricityInputController extends StateNotifier<ElectricityInputState> {
 
   final Reader _read;
 
-  Future<void> addEcoLog() async {
-    ////TODO: Firestoreとつなげる
+  Future<void> addEcoLog({
+    required String name,
+    required CategoryType type,
+    required String unitValue,
+    required String time,
+    required String date }) async {
     state = state.copyWith(exception: null, isLoading: true);
+    ////TODO: Firestoreとつなげる
+    final entity = EcoLogEntity.toEcoLogEntity(
+        name: name,
+        value: int.parse(unitValue),
+        categoryType: type,
+        time: int.parse(time),
+        date: date.toDateTime()
+    );
     return Future.delayed(const Duration(seconds: 2), () {
       state = state.copyWith(exception: null, isLoading: false);
     });
