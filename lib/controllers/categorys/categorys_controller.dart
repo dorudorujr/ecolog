c3031fde21f1f9555afd91f00d82512573d0fb41
杉岡成哉 <a1209s518n928@gmail.com>
2021-09-12 14:33:49 +0900

    MockからFirestoreに変更

--- lib/controllers/categorys/categorys_controller.dart
+++ lib/controllers/categorys/categorys_controller.dart
@@ -3,6 +3,7 @@ import 'package:state_notifier/state_notifier.dart';
 
 import 'package:ecolog/controllers/categorys/categorys.dart';
 import 'package:ecolog/application_model/mock/provider/firestore/mock_firestore.dart';
+import 'package:ecolog/application_model/firebases/firestore/category/category.dart';
 
 final categorysControllerProvider = StateNotifierProvider<CategorysController, CategorysState>(
     (ref) => CategorysController(ref.read),
@@ -10,8 +11,7 @@ final categorysControllerProvider = StateNotifierProvider<CategorysController, C
 
 class CategorysController extends StateNotifier<CategorysState> {
   CategorysController(this._read) : super(CategorysState()) {
-    ///TODO: Mockなので本番に変える
-    _read(categoryDaoMockProvider).getCategories().then((value) => {
+    _read(categoryDaoProvider).getCategories().then((value) => {
       state = state.copyWith(categorys: value)
     });
   }
