eb7ff06e878bfe13a9fbce26434e28d81b0fc1c9
杉岡成哉 <a1209s518n928@gmail.com>
2021-09-25 16:04:33 +0900

    EcologInputControllerのaddEcologメソッドのテストケース追加

--- test/controllers/ecolog_input_controller_test.dart
+++ test/controllers/ecolog_input_controller_test.dart
@@ -2,8 +2,28 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'package:ecolog/controllers/ecolog_input/ecolog_input.dart';
+import 'package:ecolog/application_model/mock/provider/firestore/mock_firestore.dart';
+import 'package:ecolog/application_model/models/models.dart';
+import 'package:ecolog/application_model/firebases/firestore/ecolog/ecolog.dart';
 
 void main() {
+  test('addEcolog', () async {
+    final container = ProviderContainer(
+      overrides: [
+        ecoLogDaoProvider.overrideWithProvider(Provider((ref) => ref.read(ecoLogDaoMockProvider)))
+      ]
+    );
+    final target = container.read(ecologInputProvider.notifier);
+    expect(target.debugState.isLoading, false);
+    expect(target.debugState.exception, null);
+    target.addEcolog(name: "", type: CategoryType.water, unit: '1').then((_) {
+      expect(target.debugState.isLoading, false);
+    });
+    /// Mockで2秒待機することは確定なのでここはtrueになる
+    expect(target.debugState.isLoading, true);
+    expect(target.debugState.exception, null);
+  });
+
   test('setUnit', () async {
     final container = ProviderContainer(overrides: []);
     final target = container.read(ecologInputProvider.notifier);
